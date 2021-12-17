765be9b323ee338f42cb54495e229e0973dd39d2
LiveWire84 <76985198+LiveWire84@users.noreply.github.com>
2021-12-17 11:54:49 +0530

    Update ModularNeuralNetwork.py

--- ModularNeuralNetwork.py
+++ ModularNeuralNetwork.py
@@ -1,92 +1,91 @@
 import numpy as np
+import pandas as pd
 
 
-# Sigmoid func, Takes in Array to apply sigmoid func
+def tanh(Z):
+    return (np.e**(Z) - np.e**(-Z))/(np.e**Z + np.e**(-Z))
 
 
-def g(Z):
-    return 1 / (1 + np.e ** (-Z))
+def sig(Z):
+    return 1/(1 + np.e**(-Z))
 
 
-def eta(x):
-    ETA = 0.0000000001
-    return np.maximum(x, ETA)
-# Core of the Neural Network
+def LeakyReLu(Z):
+    n = []
+    for i in range(len(Z)):
+        n.append(np.maximum(0.01*Z[i], Z[i]))
+    return n
 
 
-class NeuralNet:
-    #  declaration of Neural Network, x takes in
-    def __init__(self, x, y, h):
-        self.x = x
-        self.y = y
-        self.dim = np.ones(len(h) + 2)
-        self.dim = self.dim.astype(int)
-        self.dim[0] = len(x[0])
-        self.dim[1:len(h) + 1] = h
-        self.dim[-1] = len(y[0])
-        self.q = []
-        self.d = []
+def ReLu(Z):
+    return np.log(1 + np.e**Z)
+
+
+class MNN:
+    def __init__(self, X, Y, h):
+        self.X = X
+        self.Y = Y
+        self.dim = np.ones(len(h)+2).astype(int)
+        self.dim[0] = len(self.X[0])
+        self.dim[1:len(h)+1] = h
+        self.dim[-1] = len(Y[0])
+        self.w = []
+        self.dw = []
         self.db = []
+        self.dz = []
         self.b = []
-        self.beckoning = []
-        self.val = 0
+        self.a = []
+        self.acc = 0
         for i in range(len(self.dim) - 1):
-            self.q.append(100*np.random.rand(self.dim[i + 1], self.dim[i]))
-            self.d.append(np.zeros((self.dim[i + 1], self.dim[i])))
-            self.b.append(100*np.random.rand(self.dim[i + 1], 1))
+            self.w.append(np.random.rand(self.dim[i + 1], self.dim[i])*0.01)
+            self.dw.append(np.zeros((self.dim[i + 1], self.dim[i])))
+            self.b.append(np.random.rand(self.dim[i + 1], 1)*0.0001)
             self.db.append(np.zeros((self.dim[i + 1], 1)))
-            self.beckoning.append(False)  #
+            self.dz.append(np.zeros((self.dim[i + 1], self.dim[i])))
 
-    def forwardAndBackPropagate(self, l):
-        for i in range(len(self.x)):
-            a = [np.array(self.x[i], ndmin=2).T]
-            for j in range(len(self.q)):
-                a.append(g(self.q[j] @ a[j] + self.b[j]))
-            self.val = [a[-1], self.y[i]]
-            sigma = [(a[-1] - np.array(self.y[i], ndmin=2).T)]
-            for j in range(1, len(self.q)):
-                sigma.insert(0, (self.q[-j].T @ sigma[0]) * a[-j - 1] * (1 - a[-j - 1]))
-            for j in range(len(sigma)):
-                self.db[j] = sigma[j]
-                self.d[j] = (sigma[j] @ a[j].T)
-            self.calcQ(l)
+    def ForwardPropagate(self, ):
+        self.a = []
+        self.a.append(self.X.T)
+        for i in range(len(self.w) - 1):
+            self.a.append(sig(self.w[i]@self.a[-1] + self.b[i]))
+        self.a.append(sig(self.w[-1]@self.a[-1] + self.b[-1]))
 
+    def BackPropagate(self):
+        self.dz[-1] = (self.a[-1] - self.Y.T)
+        self.dw[-1] = self.dz[-1]@self.a[-2].T/len(self.X)
+        self.db[-1] = np.sum(self.dz[-1], axis=1, keepdims=True)/len(self.X)
+        for i in range(-2, -(len(self.dw)+1), -1):
+            self.dz[i] = (self.dz[i+1].T@self.w[i+1]).T*self.a[i]*(1 - self.a[i])
+            self.dw[i] = self.dz[i]@self.a[i-1].T
+            self.db[i] = np.sum(self.dz[i], axis=1, keepdims=True)/len(self.X)
+        print(self.db)
 
-    def calcQ(self, l):
-        for i in range(len(self.q)):
-            self.q[i] -= l * (self.d[i]) / len(self.x)
-        for i in range(len(self.b)):
-            self.b[i] -= l * self.db[i] / len(self.x)
+    def assignDelta(self, l):
+        for i in range(len(self.w)):
+            self.w[i] -= l*self.dw[i]
+            self.b[i] -= l*self.db[i]
+
+    def trainNet(self, l):
+        print('initializing.... ')
+        self.acc = 0
+        while self.acc <= 0.9:
+            self.ForwardPropagate()
+            self.BackPropagate()
+            self.assignDelta(l)
+            self.acc = 0
+            predans = np.argmax(self.a[-1].T, axis=1)
+            actans = np.argmax(self.Y, axis=1)
+            print(predans, actans)
+            for i in range(len(predans)):
+                if predans[i] == actans[i]:
+                    self.acc += 1
+            self.acc /= len(self.X)
+            print(self.acc)
+        return 'Successful', self.w
 
     def TestNet(self, x):
-        a = [np.array(x, ndmin=2).T]
-        for j in range(len(self.q)):
-            a.append(g(self.q[j] @ a[j] + self.b[j]))
-        return a[-1]
-
-    def gradientCheck(self, e):
-        jqe = 0.00
-        jqe2 = 0.00
-        for i in range(len(self.x)):
-            a = [np.array(self.x[i], ndmin=2).T]
-            for j in range(len(self.q)):
-                a.append(g((self.q[j] + e) @ a[j] + (self.b[j] + e)))
-            af = [np.array(self.x[i], ndmin=2).T]
-            jqe -= (np.log(a[-1])*self.y[i] + np.log(eta(1 - a[-1]))*(1 - self.y[i]))
-            for j in range(len(self.q)):
-                af.append(g((self.q[j] - e) @ af[j] + (self.b[j] - e)))
-            jqe2 -= (np.log(af[-1])*self.y[i] + np.log(eta(1 - af[-1]))*(1 - self.y[i]))
-        return (jqe - jqe2)/(2 * e)
-
-    def TrainNet(self, l, debug, epochs):
-        print('initializing.... ')
-        for i in range(epochs):
-            while True:
-                self.forwardAndBackPropagate(l)
-                self.calcQ(l)
-                if debug:
-                    print(self.val)
-                    print(self.q)
-        return 'Successful', self.q
-
-
+        na = [np.array(x, ndmin=2).T]
+        for j in range(len(self.w) - 1):
+            na.append(tanh(self.w[j] @ na[-1] + self.b[j]))
+        na.append(sig(self.w[-1] @ na[-1] + self.b[-1]))
+        return na[-1]
